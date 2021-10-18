384168248db942d6697e1cc1b3aceb2c49aab7b5
hsmnzaydn <serkan.zaydn@gmail.com>
2021-10-18 19:58:25 +0300

    added app icon and disabled night mode

--- app/src/main/java/com/hsmnzaydn/satellites/SatellitesApp.kt
+++ app/src/main/java/com/hsmnzaydn/satellites/SatellitesApp.kt
@@ -1,11 +1,13 @@
 package com.hsmnzaydn.satellites
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class SatellitesApp:Application() {
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
